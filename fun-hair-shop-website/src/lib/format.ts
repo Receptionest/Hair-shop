4f6f01720788fd97fa7e906f3dd05471c06ab913
Receptionest <eddytheveteran@gmail.com>
2026-09-17 09:13:45 +0200

    Delete fun-hair-shop-website directory

--- fun-hair-shop-website/src/lib/format.ts
+++
@@ -1,9 +0,0 @@
-export const WHATSAPP_NUMBER = "27823361947";
-
-export function formatRand(value: number) {
-  return `R${value.toLocaleString("en-ZA")}`;
-}
-
-export function fromPrice(options: { label: string; price: number }[]) {
-  return Math.min(...options.map((o) => o.price));
-}
